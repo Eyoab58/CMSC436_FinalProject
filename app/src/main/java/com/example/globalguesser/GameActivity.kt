0f0b94943b7b32aac1026293df8c79cba79ba692
robel0 <123598766+robel0@users.noreply.github.com>
2023-12-08 14:11:28 -0500

    Update GameActivity.kt
    Controller stuff

--- app/src/main/java/com/example/globalguesser/GameActivity.kt
+++ app/src/main/java/com/example/globalguesser/GameActivity.kt
@@ -1,69 +1,83 @@
 package com.example.globalguesser
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.text.Editable
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class GameActivity : AppCompatActivity() {
+    // variables for the timer
     private lateinit var countdownText:TextView
     private lateinit var countDownTimer:CountDownTimer
-    private var timeLeftInMilliseconds:Long = 6000;
+    private var timeLeftInMilliseconds:Long = 60000
 
-    // list of flags (for order)
-    private lateinit var easyFlagsOrder : ArrayList<String> // ["us_flag",...]
-    private lateinit var mediumFlagsOrder : ArrayList<String>
-    private lateinit var hardFlagsOrder : ArrayList<String>
+    // list of flags
+    private lateinit var flagMap : HashMap<String, Int>
+    private var flags : ArrayList<String> = ArrayList()
+
+    private lateinit var flagImage : ImageView
+    private lateinit var flagText : EditText
+    private var currFlagIndex : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_view)
-
         // timer
         countdownText = findViewById(R.id.timer)
         startTimer()
 
-        // flags
-        for(e in MainActivity.easyFlags.keys){
-            easyFlagsOrder.add(e)
-        }
+        // views
+        flagImage = findViewById(R.id.flag_image)
+        flagText = findViewById(R.id.flag_text)
 
-        for(m in MainActivity.mediumFlags.keys){
-            mediumFlagsOrder.add(m)
-        }
+        // store persistent data
+        sharedPreferences = this.getSharedPreferences("Game", Context.MODE_PRIVATE)
 
-        for(h in MainActivity.hardFlags.keys){
-            hardFlagsOrder.add(h)
-        }
-
-        // from this order, depending on difficulty, show the next flags
-        if (MainActivity.difficultyLevel == "Easy"){
-
-            for(i in easyFlagsOrder){
-                val flagToShow : ImageView = findViewById(resources.getIdentifier(i, "id", packageName))
-                flagToShow.visibility = ImageView.VISIBLE // shows flag
-
-                // flagToShow.visiblity = ImageView.GONE // to hide it
+        // get the right flags based on difficulty level
+        flagMap = when (MainActivity.difficultyLevel) {
+            "Easy" -> {
+                MainActivity.easyFlags
             }
-        } else if(MainActivity.difficultyLevel == "Medium"){
-            for(i in mediumFlagsOrder){
-                val flagToShow : ImageView = findViewById(resources.getIdentifier(i, "id", packageName))
-                flagToShow.visibility = ImageView.VISIBLE // shows flag
 
-                // flagToShow.visiblity = ImageView.GONE // to hide it
+            "Medium" -> {
+                MainActivity.mediumFlags
             }
-        } else {
-            for(i in hardFlagsOrder){
-                val flagToShow : ImageView = findViewById(resources.getIdentifier(i, "id", packageName))
-                flagToShow.visibility = ImageView.VISIBLE // shows flag
 
-                // flagToShow.visiblity = ImageView.GONE // to hide it
+            else -> {
+                MainActivity.hardFlags
             }
         }
+        for(e in flagMap.keys) {
+            flags.add(e)
+        }
 
+        // set the first flag
+        flagMap[flags[0]]?.let { flagImage.setBackgroundResource(it) }
+        // initialize game variable
+        game = Game(flags[0], sharedPreferences.getLong("bestTime", 100))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopTimer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopTimer()
+    }
+
+    private fun stopTimer() {
+        countDownTimer?.cancel()
     }
 
     private fun startTimer() {
@@ -80,6 +94,44 @@ class GameActivity : AppCompatActivity() {
         }.start()
     }
 
+    // check if answer is correct when ENTER is clicked
+    fun checkAnswer(v : View) {
+        if(game.isGuessCorrect(flagText.text.toString())) {
+            flagText.setText("")
+            flags.removeAt(currFlagIndex)
+
+            if (flags.isEmpty() || flags.size < 1)
+                modifyData()
+            else if (currFlagIndex > flags.size - 1) {
+                game.setCurrFlag(flags[currFlagIndex])
+                flagMap[flags[currFlagIndex]]?.let { flagImage.setBackgroundResource(it) }
+            } else {
+                game.setCurrFlag(flags[currFlagIndex])
+                flagMap[flags[currFlagIndex]]?.let { flagImage.setBackgroundResource(it) }
+            }
+        } else {
+            flagText.setText("")
+            Toast.makeText(this, "Wrong Answer", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // move to a new flag when SKIP is clicked
+    fun skipFlag(v : View) {
+        if(game.getNumFlagsGuessed() == 5) {
+            modifyData()
+        } else if(flags.size == 1) {
+            Toast.makeText(this, "Last Remaining Flag", Toast.LENGTH_LONG).show()
+        } else if(currFlagIndex == flags.size - 1) {
+            currFlagIndex = 0
+            game.setCurrFlag(flags[currFlagIndex])
+            flagMap[flags[currFlagIndex]]?.let { flagImage.setBackgroundResource(it) }
+        } else {
+            currFlagIndex += 1
+            game.setCurrFlag(flags[currFlagIndex])
+            flagMap[flags[currFlagIndex]]?.let { flagImage.setBackgroundResource(it) }
+        }
+    }
+
     private fun updateTimer() {
         val minutes = (timeLeftInMilliseconds / 1000) / 60
         val seconds = (timeLeftInMilliseconds / 1000) % 60
@@ -90,7 +142,16 @@ class GameActivity : AppCompatActivity() {
 
 
     fun modifyData(){
-        var intent : Intent = Intent (this, GameOver::class.java)
+        // store how long it took to finish this round
+        game.setCurrentTime(60 - (timeLeftInMilliseconds / 1000))
+
+        // jump to game over screen
+        val intent : Intent = Intent (this, GameOver::class.java)
         startActivity(intent)
+    }
+
+    companion object {
+        lateinit var game : Game
+        lateinit var sharedPreferences : SharedPreferences
     }
 }
